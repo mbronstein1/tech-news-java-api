376ec4c7bbf9e167fde6bfeeddc237ec413b0977
Matthew Bronstein <mbronstein1@gmail.com>
2023-01-17 17:15:26 -0600

    completed controllers

--- src/main/java/com/technews/controller/CommentController.java
+++ src/main/java/com/technews/controller/CommentController.java
@@ -1,5 +1,41 @@
 package com.technews.controller;
 
-public class CommentController {
+import com.technews.model.Comment;
+import com.technews.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+public class CommentController {
+    @Autowired
+    CommentRepository repository;
+
+    @GetMapping("/api/comments")
+    public List<Comment> getAllComments() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/api/comments/{id}")
+    public Comment getComment(@PathVariable int id) {
+        return repository.getById(id);
+    }
+
+    @PostMapping("/api/comments")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment createComment(@RequestBody Comment comment) {
+        return repository.save(comment);
+    }
+
+    @PutMapping("/api/updateComment")
+    public Comment updateComment(@RequestBody Comment comment) {
+        return repository.save(comment);
+    }
+
+    @DeleteMapping("/api/comments/{id}")
+    public void deleteComment(@PathVariable int id) {
+        repository.deleteById(id);
+    }
 }
